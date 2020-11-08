199c1c2755209b2ba677d0ca049a3c796ba47eda
Ion Babalau <ionbabalau2020@gmail.com>
2020-11-08 03:51:58 +0200

    Add wikipedia culture and politics

--- bot/src/main/kotlin/com/justai/jaicf/template/scenario/Connector.kt
+++ bot/src/main/kotlin/com/justai/jaicf/template/scenario/Connector.kt
@@ -48,27 +48,32 @@ object Connector {
         val query = JSONObject(URL("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext&exsectionformat=plain&titles=$city&redirects=").readText()).get("query").toString()
         var startIndex = query.indexOf("\\n\\n\\nHistory", 0)
         startIndex += 15
-        var stopIndex = query.indexOf("\\n\\n\\n", startIndex)
+        val stopIndex = query.indexOf("\\n\\n\\n", startIndex)
         query.substring(startIndex, stopIndex)
     }
 
     fun getWikiCityCultureInfo(city: String) = runBlocking {
-        ""
+        val query = JSONObject(URL("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext&exsectionformat=plain&titles=$city&redirects=").readText()).get("query").toString()
+        var startIndex = query.indexOf("\\n\\n\\nCulture", 0)
+        startIndex += 15
+        val stopIndex = query.indexOf("\\n\\n\\n", startIndex)
+        query.substring(startIndex, stopIndex)
     }
 
     fun getWikiCityPoliticsInfo(city: String) = runBlocking {
-        ""
+        val query = JSONObject(URL("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext&exsectionformat=plain&titles=$city&redirects=").readText()).get("query").toString()
+        var startIndex = query.indexOf("\\n\\n\\nPolitics", 0)
+        startIndex += 16
+        val stopIndex = query.indexOf("\\n\\n\\n", startIndex)
+        query.substring(startIndex, stopIndex)
     }
 
     fun getCity(latitude: Double, longitude: Double): String {
         val url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&result_type" +
                 "=administrative_area_level_1&key=AIzaSyC9umGSBv04JS9H1mNoIUdzf8o8e_IQ_nw"
         val results = JSONArray(JSONObject(URL(url).readText()).get("results").toString())[0].toString()
-        var city_name = ""
         val address_component = JSONArray(JSONObject(results).get("address_components").toString())[0].toString()
-        city_name = JSONObject(address_component).get("long_name").toString()
-
-        return city_name
+        return JSONObject(address_component).get("long_name").toString()
     }
 
 }
