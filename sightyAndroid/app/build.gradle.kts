cb03aaa5bf26ff68447c24a8726eaaac36d076c8
Dan <cornul11@gmail.com>
2020-11-08 05:35:41 +0100

    folder refactoring, removed unnecessary stuff, renamed app and package

---
+++ sightyAndroid/app/build.gradle.kts
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+val componentsVersion: String by rootProject.extra
+val aimyboxVersion: String by rootProject.extra
+val kotlinVersion: String by rootProject.extra
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        applicationId = "org.sighty.assistant"
+        minSdkVersion(21)
+        targetSdkVersion(28)
+        versionName = componentsVersion
+        versionCode = 1
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+    }
+
+    lintOptions {
+        isCheckAllWarnings = true
+        isWarningsAsErrors = false
+        isAbortOnError = true
+    }
+}
+
+repositories {
+    mavenLocal()
+    google()
+    jcenter()
+    mavenCentral()
+    maven("https://kotlin.bintray.com/kotlinx")
+}
+
+dependencies {
+
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.0-beta-3")
+
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("com.google.android.material:material:1.0.0")
+    implementation("com.google.android.gms:play-services-location:17.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
+
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.recyclerview:recyclerview:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.core:core-ktx:1.1.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.1.0")
+
+    implementation("com.squareup.okhttp3:okhttp:4.2.1")
+
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.1")
+
+    implementation("com.justai.aimybox:components:$componentsVersion")
+    implementation("com.justai.aimybox:core:$aimyboxVersion")
+    implementation("com.justai.aimybox:google-platform-speechkit:$aimyboxVersion")
+}
