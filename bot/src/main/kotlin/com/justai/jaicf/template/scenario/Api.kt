6e82bd1e7d941f99eab3051653b1631bcb98f4a7
Ion Babalau <ionbabalau2020@gmail.com>
2020-11-08 03:16:53 +0200

    Add random facts about city

--- bot/src/main/kotlin/com/justai/jaicf/template/scenario/Api.kt
+++ bot/src/main/kotlin/com/justai/jaicf/template/scenario/Api.kt
@@ -18,6 +18,22 @@ object Api {
         JSONObject(q[2].substring(0, q[2].length - 2)).get("extract").toString()
     }
 
+    fun getWikiCityHistoryInfo(city: String) = runBlocking {
+        val query = JSONObject(URL("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext&exsectionformat=plain&titles=$city&redirects=").readText()).get("query").toString()
+        var startIndex = query.indexOf("\\n\\n\\nHistory", 0)
+        startIndex += 15
+        var stopIndex = query.indexOf("\\n\\n\\n", startIndex)
+        query.substring(startIndex, stopIndex)
+    }
+
+    fun getWikiCityCultureInfo(city: String) = runBlocking {
+        ""
+    }
+
+    fun getWikiCityPoliticsInfo(city: String) = runBlocking {
+        ""
+    }
+
     private fun normalizeValue(value: String) : String{
         if (value == "University of Groningen")
             return  "University+of+Groningen"
